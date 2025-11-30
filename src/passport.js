cd15cdf5b95eddd14ca9d08977dd884071383025
isimbirehema <isimbi.rehema@ontariotechu.net>
2025-11-29 20:22:09 -0500

    Update GitHub OAuth client ID/secret

--- src/passport.js
+++ src/passport.js
@@ -11,11 +11,12 @@ passport.use(new GoogleStrategy({
   return done(null, profile);
 }));
 
-// GitHub OAuth Strategy
+// GitHub OAuth Strategy with scope fix
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL   
+  callbackURL: process.env.GITHUB_CALLBACK_URL,
+  scope: ['user:email'] // ensures GitHub returns email
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
